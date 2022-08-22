3c38ed8587b69ef3218899160b24665bc60f5237
Praveen Kumar A <pk0128331@gmail.com>
2022-08-22 11:29:32 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,30 @@ import Status from './components/Status';
 import Expense from './components/Expense'
 import './App.css'
 import ExpenseInput from './components/ExpenseInput';
+import { useContext, useEffect } from 'react';
+import { AppContext } from './context/AppContext';
 
 
 const App = () => {
 
+    const {expenses, dispatch} = useContext(AppContext);
+
+    useEffect(()=>{
+        const exp = JSON.parse(localStorage.getItem('budget-app-data'))
+        console.log('expense initilized : '+exp)
+
+        if(exp) {
+           dispatch({type : 'initialize_expense', expenses : exp}) 
+        }
+    }, []);
+
+    useEffect(()=>{
+        localStorage.setItem('budget-app-data', JSON.stringify(expenses))
+        console.log('expense modified : '+expenses)
+        dispatch({type : 'set_filtered_expense'})
+    }, [expenses])
+
     return(
-        
         <div className='container'>
             <h1 className='my-3'>Budget Planner</h1>
             <Status />
@@ -17,7 +35,6 @@ const App = () => {
             <h2 className='my-3'>Add Expense</h2>
             <ExpenseInput />
         </div>
-        
     )
 }
 
